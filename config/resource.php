85194bdf6f6898e54d2efae999b812de176c382c
Suman Shrestha <summonshr@gmail.com>
2022-02-28 19:12:18 +0530

    WIP

--- config/resource.php
+++ config/resource.php
@@ -1,15 +1,6 @@
 <?php
 
-use App\Models\Hotel;
-use App\Models\Office;
-use App\Models\Trip;
-use App\Models\Vacation;
-
 return [
     'types' => [
-        'office' => Office::class,
-        'trip' => Trip::class,
-        'hotel' => Hotel::class,
-        'vacation' => Vacation::class,
     ]
 ];
