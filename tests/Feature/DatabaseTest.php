be26dff5486ab5ae026f174939c350e4e471d184
Suman Shrestha <summonshr@gmail.com>
2022-02-28 19:29:57 +0530

    Remove tests

--- tests/Feature/DatabaseTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-test('Database has seeded data', function () {
-    $this->seed();
-    // check database has users
-    $this->assertDatabaseCount('users',21)->assertDatabaseCount('trips',75)->assertDatabaseCount('offices',15);
-});
